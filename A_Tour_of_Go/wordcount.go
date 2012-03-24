dbfaf55e0033c8bb8e29999dbf5686b3a0e1458d
Vermee81 <hrksb5029@gmail.com>
2012-03-24 15:23:13 +0900

    Adding a wordcount program. It is from Lesson44 in The Tour of Go.

---
+++ A_Tour_of_Go/wordcount.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+    "tour/wc"
+    "strings"
+)
+
+func WordCount(s string) map[string]int{
+    var me map[string]int
+    m = make(map[string]int)
+    splitted := strings.Fields(s)
+    for _, str := range splitted {
+        m[str] = m[str] + 1
+    }
+    return m
+}
+
+func main(){
+    wc.Test(WordCount)
+}
+
